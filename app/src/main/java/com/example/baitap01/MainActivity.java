95f7b0dc440f2b1e8f90db05e975b4de9555b450
binh1922004 <binh1920042018@gmail.com>
2025-01-17 09:03:53 +0700

    add chan le

--- app/src/main/java/com/example/baitap01/MainActivity.java
+++ app/src/main/java/com/example/baitap01/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.baitap01;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -10,6 +12,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     EditText edtArray;
     Button btnOddEven;
@@ -17,17 +23,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
 
         edtArray = findViewById(R.id.edtArray);
         btnOddEven = findViewById(R.id.btnOddEven);
 
+        btnOddEven.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String[] numbers = edtArray.getText().toString().split(" ");
+                List<Integer> numberList = new ArrayList<>();
+                // Chuyển từng phần tử từ chuỗi sang số nguyên và thêm vào danh sách
+                for (String num : numbers) {
+                    numberList.add(Integer.parseInt(num));
+                }
+                logOddEven(numberList);
+            }
+        });
 
 
-        setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private void logOddEven(List<Integer> numberList) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int x: numberList){
+            if (!map.containsKey(x)){
+                map.put(x, 1);
+            }
+            else{
+                map.put(x, map.get(x) + 1);
+            }
+        }
+        for (int x: numberList){
+            if (map.get(x) == 1){
+                if (x % 2 == 0){
+                    Log.d("So rieng biet", "So chan: " + x);
+                }
+                else{
+                    Log.d("So rieng biet", "So le: " + x);
+                }
+            }
+        }
     }
 }
