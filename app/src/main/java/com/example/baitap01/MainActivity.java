d14f500a0ac8508aa38e810f23cc107db3a4a7ad
binh1922004 <binh1920042018@gmail.com>
2025-01-17 08:47:18 +0700

    Initial commit

---
+++ app/src/main/java/com/example/baitap01/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.baitap01;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edtArray;
+    Button btnOddEven;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+
+        edtArray = findViewById(R.id.edtArray);
+        btnOddEven = findViewById(R.id.btnOddEven);
+
+
+
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
